57d6f25334883cb1b62ce0f28cf2cfdaf049f209
Axat30 <dhimmaraxat@gmail.com>
2025-09-12 20:01:06 +0530

    match the navbar of mentor to admin

--- src/components/MentorDashboard/components/MentorDashboardLayout.jsx
+++ src/components/MentorDashboard/components/MentorDashboardLayout.jsx
@@ -1,17 +1,19 @@
 // src/components/MentorDashboard/MentorDashboardLayout.jsx
-import React from "react";
-import Navbar from "./Navbar";
+import React, { useState } from "react";
 import Footer from "./Footer";
 import { Outlet } from "react-router-dom";
+import Navbar from "./Navbar";
 
 const MentorDashboardLayout = () => {
+const [sidebarOpen, setSidebarOpen] = useState(true);
+
   return (
     <div className="flex min-h-screen">
       {/* Sidebar/Navbar */}
       <Navbar />
 
       {/* Content Area */}
-      <div className="flex flex-col flex-1">
+      <div className={`flex flex-col flex-1 transition-all duration-300 ${sidebarOpen ? "ml-64" : "ml-0"}`}>
         <main className="flex-1 py-12 px-8 text-center bg-gray-50 text-gray-900 font-sans">
           {/* This is where child pages will render */}
           <Outlet />
